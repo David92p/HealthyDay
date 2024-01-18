f17398c0d45337ce50346c73330eed62833fa921
David92p <dave.panetta92@gmail.com>
2024-01-18 17:09:43 +0100

    Added my colors configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "greenbox": "#1F2B21", 
+        "mypink": "#FFBBDC"
+      }
+    },
   },
   plugins: [],
 }
