b70dcfac4fa4b567ca6fd718ae4b5b93d8e6df94
David92p <Dave.panetta92@gmail.com>
2024-01-19 20:19:23 +0100

    Added componet beta

---
+++ src/components/Recipes.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recipes:React.FC = () => {
+  return (
+    <div className='w-full h-screen bg-mygreen text-mypink text-5xl'>
+        COMPONENTE RICERCA RICETTE .....
+    </div>
+  )
+}
+
+export default Recipes
