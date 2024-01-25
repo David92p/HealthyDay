0e59f03d19a0b16058eb11c74f23b8bcb354ef8a
David92p <Dave.panetta92@gmail.com>
2024-01-25 21:11:43 +0100

    Added import

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import Recipes from "./Recipes";
 import Contact from "./Contact";
 import About from "./About"
 import Footer from "./Footer";
+import Research from "./Research";
+import RecipeCard from "./RecipeCard";
 
-export { Home, Recipes, Contact, About, Footer }
+export { Home, Recipes, Contact, About, Footer, Research, RecipeCard }
