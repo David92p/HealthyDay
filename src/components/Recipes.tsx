4b9c76a9fb298e647f199c556305640e53a59ab4
David92p <Dave.panetta92@gmail.com>
2024-01-25 21:12:53 +0100

    Added components recipes

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Research } from '.'
+import Carousel from './Carousel'
+import nondisponibile from "../assets/global/nondisponibile.png"
+
+type RecipeCardType = {
+  id: number
+  title: string
+  image: string
+}
 
 const Recipes:React.FC = () => {
+
+  const [cards, setCards] = useState<RecipeCardType[]>([])
+
+  const getRec = async () => {
+    const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9&include-tags=vegetarian`)
+    const data = response.data.recipes.map((recepi: RecipeCardType):RecipeCardType => {
+      return {id: recepi.id, title: recepi.title, image: recepi.image || nondisponibile}
+    })
+    
+    setCards(data)
+  }
+
+  useEffect(() => {
+    getRec()
+  }, [])
+
+
   return (
-    <div className='w-full h-screen bg-mygreen text-mypink text-5xl'>
-        COMPONENTE RICERCA RICETTE .....
+    <div className='flex flex-col w-full bg-mygreen'>     
+      <Research type={"Ricetta"}/>
+      { <Carousel cards={cards} /> }
     </div>
   )
 }
--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -1,9 +1,19 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-const Research:React.FC = () => {
+type ResearchType = {
+  type: "Ingrediente" | "Ricetta"
+}
+
+const Research:React.FC<ResearchType> = ({ type }) => {
   return (
-    <div>
-      
+    <div className='w-full flex flex-col p-6 gap-2 cursor-default'>
+      <label className='text-3xl tracking-wider text-center text-mypink'>Cerca {type}</label>
+      <input type="text"placeholder={`Pasta pomodoro ...`} maxLength={30} className={`h-10 bg-slate-100 text-mygreen text-3xl px-2 placeholder:text-2xl py-2 rounded-md`}/>
+      <button className='bg-mypink text-mygreen w-36 sm:w-40 h-8 sm:h-10 rounded-sm mx-auto text-2xl sm:text-3xl' >
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </button>
     </div>
   )
 }
