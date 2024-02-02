839743fe75722e6ab54464a4a63f04abcfed9c2c
David92p <dave.panetta92@gmail.com>
2024-02-02 17:19:32 +0100

    added new routes

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,5 +1,7 @@
 import { useRoutes } from "react-router-dom";
-import { Home, Contact, Recipes, About } from "../components/"
+import { Home, Ingredients, Recipes, Details, Planner, Contact, About, Breakfast, Dinner, Lunch } from "../components/"
+
+
 
 const Router:React.FC = () => {
   return useRoutes([
@@ -8,12 +10,36 @@ const Router:React.FC = () => {
         element: <Home />
     }, 
     {
-        path: "ricettario",
-        element: <Recipes/>
+      path: "ingredienti",
+      element: <Ingredients/>
     },
     {
-        path: "contattaci",
-        element: <Contact/>
+      path: "ricettario",
+      element: <Recipes/>
+    },
+    {
+      path: "/ricettario/dettaglio/:id",
+      element: <Details />
+    },
+    {
+      path: "ricettario/colazione",
+      element: <Breakfast/>
+    },
+    {
+      path: "ricettario/pranzo",
+      element: <Lunch/>
+    },
+    {
+      path: "ricettario/cena",
+      element: <Dinner/>
+    },
+    {
+      path: "pianifica",
+      element: <Planner/>
+    },
+    {
+      path: "contattaci",
+      element: <Contact/>
     },
     {
       path: "about",
