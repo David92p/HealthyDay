946219795a97de0e62d6de4211891b3a5031606e
David92p <dave.panetta92@gmail.com>
2024-02-07 16:23:50 +0100

    text changed

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -15,22 +15,22 @@ const Home:React.FC = () => {
   return (
    <div className='flex flex-col cursor-default' style={{fontFamily: "Salsa"}}>
       {/* introduzione */}
-      <div className='flex flex-col sm:flex-row h-auto 2xl:h-[600px] w-full bg-mygreen py-8 sm:py-14 2xl:py-40 px-4 sm:px-10 2xl:px-16'>
+      <div className='flex flex-col sm:flex-row h-auto 2xl:h-[600px] w-full bg-mygreen pt-4 sm:py-16 2xl:py-40 px-4 sm:px-8 2xl:px-10'>
         <span className='align-text-top sm:w-1/3 text-mypink text-3xl sm:text-5xl 2xl:text-7xl sm:mr-5'>Ciao! <br /> Siamo su <br />Buongiorno Salute</span>
-        <span className='sm:w-2/3 text-slate-100 tracking-wider text-xl sm:text-3xl mt-6 mb-2 sm:mt-0 sm:ml-5'>
+        <span className='sm:w-2/3 text-slate-100 tracking-wider text-xl sm:text-3xl mt-4 mb-4 sm:mt-0 sm:ml-5'>
           Ci siamo appena trasferiti nel quartiere, ma è come se fossimo qui da sempre! Ci avete accolto come vecchi amici, passando con sorrisi pronti e appetito desideroso.
           <br />Ecco perché promettiamo di prenderci cura della tua salute tramite prodotti sani e serviti con sorrisi calorosi.<br /> Siamo una famiglia adesso. Ed è così che cuciniamo per chi amiamo.
           <br />Benvenuto a casa.
         </span>
       </div>
       {/* spazio main con ricette link */}
-      <div className='flex flex-col sm:flex-row-reverse h-auto w-full bg-slate-100 pt-8 sm:pt-16 2xl:pt-24 px-4 sm:px-10 2xl:px-16'>
+      <div className='flex flex-col sm:flex-row-reverse h-auto w-full bg-slate-100 pt-8 sm:pt-16 2xl:pt-24 px-4 sm:px-6 2xl:px-10'>
         <div className='sm:w-3/5'>
           <img src={home1} alt="home2" className='brightness-75 2xl:brightness-50 object-cover h-full 2xl:h-[700px] w-full'/>
         </div>
         <div className='flex flex-col sm:w-4/5 2xl:w-2/5 text-mygreen mt-6 sm:mt-0 sm:mr-5'>
           <span className='text-3xl sm:text-5xl 2xl:text-7xl'>Inizia da quì</span>
-          <span className='text-xl sm:text-3xl mt-4'>Lavoriamo tutti insieme per portare sulla tua tavola il gusto della salute.<br/>Prova le nostre specilità iniziando da quì.</span>
+          <span className='text-xl sm:text-3xl mt-4'>Lavoriamo tutti insieme per portare sulla tua tavola sapori sani e genuini.<br/>Prova le nostre specilità iniziando da quì.</span>
           <br />
           <ul className='text-xl sm:text-3xl list-disc ml-6'>
             <li className='mb-2'>Sano come vuoi tu</li>
@@ -42,11 +42,11 @@ const Home:React.FC = () => {
         </div>
       </div>
       {/* spazio pasti 3 card  */}
-      <div className='flex flex-col sm:flex-row 2xl:justify-around sm:h-[700px] w-full bg-slate-100 pt-8 sm:py-16 px-4 sm:px-10 2xl:px-16 sm:gap-4 text-xl sm:text-3xl text-mygreen'>
+      <div className='flex flex-col sm:flex-row 2xl:justify-between sm:h-[700px] w-full bg-slate-100 pt-8 sm:pb-6 px-4 sm:px-6 2xl:px-10 sm:gap-4 text-xl sm:text-3xl text-mygreen'>
         <motion.div 
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer'
+          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer sm:mt-4 2xl:mt-10'
         >
           <img className='brightness-75 2xl:brightness-50 2xl:hover:brightness-100 object-cover h-full w-full' src={home2} />
           <button className='bg-mypink h-12'>Colazione</button>
@@ -54,7 +54,7 @@ const Home:React.FC = () => {
         <motion.div 
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer mt-4 sm:mt-0'
+          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer mt-4 2xl:mt-10'
         >
           <img className='brightness-75 2xl:brightness-50 2xl:hover:brightness-100 object-cover h-full w-full' src={home3} />
           <button className='bg-mypink h-12'>Pranzo</button>
@@ -62,21 +62,22 @@ const Home:React.FC = () => {
         <motion.div 
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer my-4 sm:mt-0'>
+          className='flex flex-col sm:w-1/3 2xl:h-[550px] 2xl:w-[400px] cursor-pointer my-4 sm:mt-4 sm:mb-0 2xl:mt-10'
+        >
           <img className='brightness-75 2xl:brightness-50 2xl:hover:brightness-100 object-cover h-full w-full' src={home4} />
           <button className='bg-mypink h-12'>Cena</button>
         </motion.div>
       </div>
       {/* spazio about e collegamenti  */}
-      <div className='flex flex-col sm:flex-row h-auto 2xl:h-[550px] w-full bg-mygreen py-8 sm:py-14 2xl:py-auto px-4 sm:px-10 2xl:px-16'>
-        <span className='sm:w-2/5 align-text-top text-mypink text-3xl sm:text-5xl 2xl:text-7xl sm:mr-5'>Colorati di Sano</span>
+      <div className='flex flex-col sm:flex-row h-auto 2xl:h-[550px] w-full bg-mygreen py-8 sm:py-14 2xl:py-auto px-4 sm:px-6 2xl:px-10'>
+        <span className='sm:w-2/5 align-text-top text-mypink text-3xl sm:text-5xl 2xl:text-7xl sm:mr-5'>Colorati di Genuino</span>
         <div className='sm:w-3/5 flex flex-col text-slate-100 mt-6 sm:mt-0'>
           <span className='text-2xl sm:text-4xl 2xl:text-6xl'>Consulta</span>
           <span className='text-lg sm:text-2xl sm:mt-4'>Cerca le ricetta che soddisfino il tuo palato!</span>
           <span className='text-2xl sm:text-4xl 2xl:text-6xl mt-5'>Prepara</span>
           <span className='text-lg sm:text-2xl sm:mt-4'>Procurati gli ingredienti consigliati e accendi i fornelli, il tuo corpo aspetta di colorarsi di sano!</span>
           <span className='text-2xl sm:text-4xl 2xl:text-6xl mt-5'>Gusta</span>
-          <span className='text-lg sm:text-2xl sm:mt-4'>Rilassati e gusta il tuo piatto, che sia un break veloce o un pasto principale. Gusta la Salute!</span>
+          <span className='text-lg sm:text-2xl sm:mt-4'>Rilassati e gusta il tuo piatto, che sia un break veloce o un pasto principale. Gusta la bontà!</span>
         </div>
       </div>
       {/* Saluti  */}
@@ -84,32 +85,6 @@ const Home:React.FC = () => {
         <img src={home5} alt="home1" className='brightness-50 object-cover h-full w-full'/>
         <h1 className='text-mypink text-3xl sm:text-5xl 2xl:text-7xl font-bold tracking-wider absolute text-center my-14 sm:my-16 mx-4 sm:mx-0'>Ti aspettano pasti<br/>sani e genuini.<br/><br/>A presto!</h1>
       </div>
-      {/* footer e contatti */}
-      {/* <div className='flex flex-col bg-mypink w-full h-auto  text-mygreen pt-6 sm:pt-14 2xl:pt-16'>
-        <span className='text-center text-3xl sm:text-5xl 2xl:text-7xl tracking-wider font-bold'>Buongiorno<br/>Salute</span>
-        <div className='flex w-full mt-8'>
-          <div className='w-1/3 flex flex-col justify-start text-cente text-mygreenr'>
-            <span className='text-xl sm:text-4xl 2xl:text-6xl text-center text-mygreen'>Seguici</span>
-            <div className='flex flex-col sm:flex-row gap-4 w-full justify-center mt-2 pl-2 mb-5'>
-              <FontAwesomeIcon icon={faLinkedin} className='fa-2x cursor-pointer'/>
-              <FontAwesomeIcon icon={faInstagram} className='fa-2x cursor-pointer'/>
-              <FontAwesomeIcon icon={faGithub} className='fa-2x cursor-pointer'/>
-            </div>
-          </div>
-          <div className='w-1/3 flex flex-col justify-start text-center text-mygreen text-xl'>
-            <span className='text-xl sm:text-4xl 2xl:text-6xl text-mygreen'>Indirizzo</span>
-            Buongiorno Salute<br/>
-            743 Freedom Lane
-            Modesto, California<br/>
-            USA 12345
-          </div>
-          <div className='w-1/3 flex flex-col justify-start text-center text-mygreen'>
-            <span className='text-xl sm:text-4xl 2xl:text-5xl text-mygreen'>Contattaci</span>
-            <FontAwesomeIcon icon={faEnvelope} className='fa-2x cursor-pointer' onClick={() => navigate("/contattaci")}/>
-          </div>
-        </div>
-        <div className='cursor-default flex items-end justify-center pt-4 pb-2 w-full sm:text-lg bg-mypink text-mygreen'>© 2024 | Davide Panetta</div>
-      </div> */}
    </div> 
   )
 }
--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -13,16 +13,16 @@ const Navbar:React.FC = () => {
   return (
     <div className='flex flex-col w-full h-48 sm:h-64 2xl:h-[500px] relative cursor-default' style={{fontFamily: "Salsa"}}>
       <img src={headerImg} alt="home1" className={`brightness-75 2xl:brightness-50 object-cover h-full w-full ${menu ? "opacity-50" : "opacity-100"}`}/>
-      <h1 style={{fontFamily: "Salsa"}} className='text-slate-100 absolute w-full text-end mt-4 pr-4 2xl:pr-16 text-xl sm:text-4xl opacity-100' onClick={() => navigate("/")}>Buongiorno Salute</h1>
+      <h1 style={{fontFamily: "Salsa"}} className='text-slate-100 absolute w-full text-end mt-4 pr-4 sm:pr-6 2xl:pr-10 text-xl sm:text-4xl opacity-100' onClick={() => navigate("/")}>Buongiorno Salute</h1>
       <div className='w-full flex justify-between items-end absolute bottom-0 2xl:pb-6'>
-        <h1 className='text-mypink text-3xl sm:text-5xl 2xl:text-8xl pl-4 2xl:pl-16 pb-2 sm:pb-14 2xl:pb-10 tracking-wider'>La sana bontà che <br /> puoi trovare</h1>
+        <h1 className='text-mypink text-3xl sm:text-5xl 2xl:text-8xl pl-4 sm:pl-6 2xl:pl-10 pb-2 sm:pb-14 2xl:pb-10 tracking-wider'>La sana bontà che <br /> puoi trovare</h1>
         <div onClick={() => setMenu(!menu)} className={`sm:hidden pr-4 pb-2 sm:pb-4`}>
           { menu 
             ? <FontAwesomeIcon icon={faXmark} className='text-slate-100 fa-2x' /> 
             : <FontAwesomeIcon icon={faBars} className='text-slate-100 fa-2x' />
           }
         </div>
-        <div className='hidden sm:flex text-slate-100 text-2xl 2xl:text-4xl sm:justify-end gap-5 2xl:gap-8 absolute bottom-0 w-full pr-10 2xl:pr-16 pb-2'>
+        <div className='hidden sm:flex text-slate-100 text-2xl 2xl:text-4xl sm:justify-end gap-5 2xl:gap-8 absolute bottom-0 w-full pr-6 2xl:pr-10 pb-2'>
           <NavLink className='text-slate-100 cursor-pointer 2xl:hover:text-mypink' onClick={() => setMenu(false)} to="/">Home</NavLink>
           <NavLink className='text-slate-100 cursor-pointer 2xl:hover:text-mypink' onClick={() => setMenu(false)} to='ingredienti'>Ingredienti</NavLink>
           <NavLink className='text-slate-100 cursor-pointer 2xl:hover:text-mypink' onClick={() => setMenu(false)} to="ricettario">Ricettario</NavLink>
