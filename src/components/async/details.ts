d93b4e0d89f2baec1485c0ecdb23dee1af0b11ac
David92p <dave.panetta92@gmail.com>
2024-02-07 16:25:47 +0100

    fixed fnc

--- src/components/async/details.ts
+++ src/components/async/details.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { DetailsType, RecipeToolType } from "../details";
 import fotoDefault from "../../assets/global/fotodefault.png"
+import { CardType } from "..";
 
 type equipmentStepType = {
     equipment: RecipeToolType[]
@@ -8,7 +9,7 @@ type equipmentStepType = {
 
 type ingredientType = {
 	id: number
-	name: string
+	nameClean: string
 	image: string
 	measures: {metric: {amount: number, unitShort: string}}
 }
@@ -16,22 +17,28 @@ type ingredientType = {
 export const getRecipeDetails= async (key:string, id:number):Promise<DetailsType | null>=> {
     try {
         const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`)
-        const { title, image, summary:description, dishTypes, servings, instructions, extendedIngredients, analyzedInstructions } = response.data
+        console.log(response.data)
+        const { title, image, summary:description, diets, servings, instructions, extendedIngredients, analyzedInstructions } = response.data
         // ingredienti 
-        const ingredients = extendedIngredients.map((ingredient:ingredientType) => {
-          if (ingredient.name) {
-            return {
-              id: ingredient.id,
-              name: ingredient.name,
-              image: image ? `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}` : fotoDefault,
+        let ingredients:RecipeToolType[] = []
+        extendedIngredients.map((ingredient:ingredientType) => {
+          if (ingredient.nameClean) {
+            const newIngredient = {
+              id: ingredient.id, 
+              name: ingredient.nameClean, 
+              image: ingredient.image ? `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}` : fotoDefault,
               measures: ingredient.measures?.metric.amount + " " + (ingredient.measures?.metric.unitShort == "" ? "qty" : ingredient.measures?.metric.unitShort )
             }
+            ingredients = ingredients.filter((element:RecipeToolType) => {
+              return element.id != newIngredient.id
+            })
+            ingredients.push(newIngredient)
           }
         })
 
         // strumenti di lavoro
         let equipments:RecipeToolType[] = []
-        analyzedInstructions[0].steps.forEach((step:equipmentStepType) => {
+        analyzedInstructions.length > 0 ? analyzedInstructions[0].steps.forEach((step:equipmentStepType) => {
             step.equipment.map((equipment:RecipeToolType) => {
               const newEquipments = {id: equipment.id, name: equipment.name, measures: null, image: image ? `https://spoonacular.com/cdn/equipment_100x100/${equipment.image}`: fotoDefault}
               equipments = equipments.filter((element:RecipeToolType) => {
@@ -39,14 +46,14 @@ export const getRecipeDetails= async (key:string, id:number):Promise<DetailsType
               })
               equipments.push(newEquipments)
             })
-        })
+        })  : null
 
         return { 
             id:response.data.id, 
             title, 
             image, 
             description, 
-            dishTypes, 
+            diets, 
             servings,
             instructions, 
             ingredients,
@@ -58,3 +65,19 @@ export const getRecipeDetails= async (key:string, id:number):Promise<DetailsType
       return null
     } 
 }
+
+// passiamo la key e un id di riferenza per le ricette da cercare
+export const getSimilarRecipes = async (key:string, id:number) => {
+  try {
+      const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/similar?apiKey=${key}&number=5`)
+      const data:CardType[] = response.data.map((recepi:CardType) => {
+          const {id, title} = recepi
+          return {id, title, image: `https://spoonacular.com/recipeImages/${id}-556x370.jpg` || fotoDefault}
+      })
+      
+      return data
+  } catch (error) {
+      return null
+  }
+}   
+
