02e8c78229af0c3c06e0734a0b3285e0e2db8212
David92p <dave.panetta92@gmail.com>
2024-02-12 12:06:54 +0100

    removed consolelog

--- src/components/async/recipes.ts
+++ src/components/async/recipes.ts
@@ -6,13 +6,11 @@ import type { CardType } from ".."
 export const getCardsRecipes = async (key:string, quantity: number):Promise<CardType[] | null>=> {
     try {
         const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${key}&number=${quantity}`)
-        //console.log(response.data)
         const data = response.data.recipes.map((recepi:CardType) => {
             const {id, title, image} = recepi
             return {id, title, image: image || fotoDefault}
         })
         return data
-        // return data
     } catch (error) {
         return null
     } 
