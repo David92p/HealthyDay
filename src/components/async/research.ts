44800e11c71e4c891f1e625fcce8402d9b7b80c6
David92p <dave.panetta92@gmail.com>
2024-02-12 11:49:32 +0100

    Added generic search function based on type

--- src/components/async/research.ts
+++ src/components/async/research.ts
@@ -1,8 +1,34 @@
 import axios from "axios"
 
 
-export const toggleResearch = async (key:string, text:string) => {
-    const response = await axios(`https://api.spoonacular.com/recipes/autocomplete?apiKey=${key}&number=20&query=${text}&include-tags=vegetarian`)
-    const x = response.data
-    console.log(x)
-  }
+export type SearchType = {
+  id: number
+  title: string
+}
+
+
+export const getResearchdata = async (key:string, type: "ingredients" | "recipe", text:string):Promise<SearchType[] | null> => {
+  const data:SearchType[] = []
+    try {
+      if (type == "recipe"){
+        const response = await axios(`https://api.spoonacular.com/recipes/autocomplete?apiKey=${key}&number=8&query=${text}`)
+        console.log(response.data)
+        response.data.map((el:SearchType) => {
+          const {id, title} = el
+          data.push({id, title})
+        })
+      } else {
+        const response =  await axios(`https://api.spoonacular.com/food/ingredients/search?apiKey=${key}&query=${text}&number=8`)
+        console.log(response.data.results)
+        response.data.results.map((el: {id: number, name: string}) => {
+          const { id, name} = el
+          data.push({id,  title: name})
+        })
+
+      }
+      return data
+    } catch (error) {
+      console.log(error)
+      return null
+    } 
+}
