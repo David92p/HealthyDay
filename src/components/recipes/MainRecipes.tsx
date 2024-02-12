49db62ad67a4a09c43a895ed4eb0c46337e870e6
David92p <dave.panetta92@gmail.com>
2024-02-12 11:51:38 +0100

    Fixed style

--- src/components/recipes/MainRecipes.tsx
+++ src/components/recipes/MainRecipes.tsx
@@ -6,28 +6,29 @@ import { useNavigate } from 'react-router-dom'
 const MainRecipes:React.FC = () => {
 	const navigate = useNavigate()
   return (
-		<>
-			<div className='flex flex-col sm:flex-row h-auto w-full bg-mygreen pt-4 sm:py-14 px-4 sm:px-6 2xl:px-10'>
-				<div className='sm:w-2/5'>
-					<p className='text-mypink text-3xl sm:text-5xl 2xl:text-7xl tracking-wider'>Inizia da quì a costruire la tua salute!</p>
-					<p className='text-slate-100 tracking-wider text-xl sm:text-3xl mt-4'>
+		<div className='flex flex-col h-auto w-full bg-pink-100' style={{fontFamily: "Salsa"}}>
+			<div className='flex flex-col sm:flex-row sm:py-6'>
+				<div className='pt-4 px-4 sm:p-6'>
+					<p className='text-mygreen text-3xl sm:text-5xl 2xl:text-7xl tracking-wider'>Inizia da quì a costruire la tua salute!</p>
+					<p className='text-mygreen tracking-wider text-xl sm:text-3xl mt-4'>
 						Consulta le migliori ricette e pianifica la tua giornata 
-						con i nostri piatti.<br/>Colazione, pranzo o cena!<br/> Cerca tutto quello che il tuo corpo richiede!
+						con i nostri piatti.<br/>Guarda i valori dei tuoi ingredienti preferiti e costruisci la tua giornata!<br/> Cerca tutto quello che il tuo corpo richiede!
 					</p>
 				</div>
-				<div className='sm:w-3/5 my-auto mt-8 sm:mt-0 sm:pl-6'>
-					<img src={first} alt="first photo" className='h-full 2xl:h-[700px] w-full object-cover brightness-100 2xl:brightness-75'/>
+			<div className='p-4 sm:p-6 2xl:w-3/5'>
+				<img src={first} alt="first photo" className='h-full w-full object-cover brightness-100 2xl:brightness-75'/>
+			</div>
+			</div>
+			<div className='flex flex-col bg-mygreen'>
+				<span className='text-center text-mypink text-3xl sm:text-5xl 2xl:text-7xl mx-auto mt-4'>
+					Consulta le nostre sezioni dedicate
+				</span>
+				<div className='flex justify-around 2xl:justify-center 2xl:gap-40 items-center w-full h-24 sm:h-36 2xl:h-44'>
+					<button className='bg-mypink h-10 sm:w-40 px-2' onClick={() => navigate("/ricettario/ingredienti")}>Ingredienti</button>
+					<button className='bg-mypink h-10 sm:w-40 px-4' onClick={() => navigate("/ricettario/pianifica")}>Pianifica</button>	
 				</div>
 			</div>
-			<span className='text-center text-mypink text-3xl sm:text-5xl 2xl:text-7xl py-4 sm:py-10'>
-				Consulta le nostre sezioni dedicate<br/> alla tua giornata
-			</span>
-			<div className="flex justify-around w-full my-4 2xl:mb-10 text-xl sm:text-2xl 2xl:text-3xl tracking-wider">
-				<button className='bg-mypink h-10 sm:w-40 px-2' onClick={() => navigate("/ricettario/colazione")}>Colazione</button>
-				<button className='bg-mypink h-10 sm:w-40 px-4' onClick={() => navigate("/ricettario/pranzo")}>Pranzo</button>
-				<button className='bg-mypink h-10 sm:w-40 px-6' onClick={() => navigate("/ricettario/cena")}>Cena</button>			
-			</div>
-		</>
+		</div>
   )
 }
 
