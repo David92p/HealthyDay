b77760dc000dbc9b69bf684078441735dcfc1d3d
David92p <Dave.panetta92@gmail.com>
2024-02-14 15:51:56 +0100

    added navigate icon ingredient

--- src/components/ingredients/IngredientIcon.tsx
+++ src/components/ingredients/IngredientIcon.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { IngredientType } from '../async'
+import { useNavigate } from 'react-router-dom'
 
 const IngredientIcon:React.FC<IngredientType> = ({id, name, image}) => {
+  const navigate = useNavigate()
+
   return (
-    <div className='flex flex-col items-center gap-4 rounded-xl w-40 sm:w-52 h-auto p-2 bg-mypink border-2 border-blue-500'>
+    <div onClick={() => navigate(`/ingredienti/dettaglio/${id}`)} className='flex flex-col items-center gap-4 rounded-xl w-40 sm:w-52 h-auto p-2 bg-mypink border-2 border-blue-500'>
       <img src={image} alt={name} className='w-20 sm:w-28 2xl:w-36 h-20 sm:h-28 2xl:h-36' />
-      <p className="text-mygreen text-xl sm:text-2xl 2xl:text-3xl tracking-wider font-bold">{name}</p>
+      <p className="text-mygreen text-center text-xl sm:text-2xl 2xl:text-3xl tracking-wider font-bold">{name}</p>
     </div>
   )
 }
