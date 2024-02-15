df8ba7ec7b8a28242d8bdff7dff74aa1d5978092
David92p <dave.panetta92@gmail.com>
2024-02-15 12:47:11 +0100

    Removed part of redirection and created Redirection component

--- src/components/recipes/MainRecipes.tsx
+++ src/components/recipes/MainRecipes.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
 import first from "../../assets/recipes/first.jpg"
-import { useNavigate } from 'react-router-dom'
 
 const MainRecipes:React.FC = () => {
-	const navigate = useNavigate()
   return (
 		<div className='flex flex-col h-auto w-full bg-pink-100' style={{fontFamily: "Salsa"}}>
 			<div className='flex flex-col sm:flex-row sm:py-6'>
@@ -18,15 +16,6 @@ const MainRecipes:React.FC = () => {
 			<div className='p-4 sm:p-6 2xl:w-3/5'>
 				<img src={first} alt="first photo" className='h-full w-full object-cover brightness-100 2xl:brightness-75'/>
 			</div>
-			</div>
-			<div className='flex flex-col bg-mygreen'>
-				<span className='text-center text-mypink text-3xl sm:text-5xl 2xl:text-7xl mx-auto mt-4'>
-					Consulta le nostre sezioni dedicate
-				</span>
-				<div className='flex justify-around 2xl:justify-center 2xl:gap-40 items-center w-full h-24 sm:h-36 2xl:h-44'>
-					<button className='bg-mypink h-10 sm:w-40 px-2' onClick={() => navigate("/ricettario/ingredienti")}>Ingredienti</button>
-					<button className='bg-mypink h-10 sm:w-40 px-4' onClick={() => navigate("/ricettario/pianifica")}>Pianifica</button>	
-				</div>
 			</div>
 		</div>
   )
