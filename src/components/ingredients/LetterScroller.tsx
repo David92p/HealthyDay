3bcb7162212d505feae41e2d595d93494412e364
David92p <dave.panetta92@gmail.com>
2024-02-15 12:45:52 +0100

    Fixed name var and fixed css

--- src/components/ingredients/LetterScroller.tsx
+++ src/components/ingredients/LetterScroller.tsx
@@ -11,28 +11,28 @@ type LetterScrollerlType = {
 }
 
 const LetterScroller:React.FC<LetterScrollerlType> = ({ scrool, setScrool, toggleSearch }) => {
-  const letters = [faA, faB, faC, faD, faE, faF, faG, faH, faI, faJ, faK, faL, faM, faN, faO, faP, faQ, faR, faS, faT, faU, faW, faX, faY, faZ]
-	const letters2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
+  const icons = [faA, faB, faC, faD, faE, faF, faG, faH, faI, faJ, faK, faL, faM, faN, faO, faP, faQ, faR, faS, faT, faU, faW, faX, faY, faZ]
+	const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
   // const [scrool, setScrool] = useState<number[]>([0, 1, 24])
    
   return (
 		<>
-			<div className='flex w-full h-32 bg-pink-100'>
-				<div className='flex justify-center items-end pb-2 w-1/5 border-4 border-red-500'>
-					<FontAwesomeIcon icon={letters[scrool[2]]} className='text-2xl' />
+			<div className='flex w-full h-32 bg-mygreen'>
+				<div className='flex justify-center items-end w-1/5 ml-4 sm:ml-6 2xl:ml-10 pb-2 border-b-2 border-mypink'>
+					<FontAwesomeIcon icon={icons[scrool[2]]} className='text-2xl text-mypink' />
 				</div>
-				<div className='flex justify-center items-end pb-2 w-3/5 border-4 border-red-500'>
-					<FontAwesomeIcon icon={letters[scrool[0]]} bounce className='text-7xl' onClick={(e) => console.log(e)}/>
+				<div className='flex justify-center items-end w-3/5 pb-2 text-mypink border-b-2 border-mypink'>
+					<FontAwesomeIcon icon={icons[scrool[0]]} className='text-7xl' onClick={(e) => console.log(e)}/>
 				</div>
-				<div className='flex justify-center items-end pb-2 w-1/5 border-4 border-red-500'>
-					<FontAwesomeIcon icon={letters[scrool[1]]} className='text-2xl' />
+				<div className='flex justify-center items-end w-1/5 mr-4 sm:mr-6 2xl:mr-10 pb-2 border-b-2 border-mypink text-mypink'>
+					<FontAwesomeIcon icon={icons[scrool[1]]} className='text-2xl' />
 				</div>
 			</div>
-			<div className='flex justify-around items-center h-14 bg-pink-100'>
+			<div className='flex justify-around items-center h-14 bg-mygreen sm:my-4 2xl:my-6'>
 				<button onClick={() => setScrool(scrool.map(letterNumber => letterNumber == 0 ? 24 : letterNumber -= 1))}  className='bg-mypink text-mygreen w-24 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl' >
 					<FontAwesomeIcon icon={faArrowRight} rotation={180} />
         </button>
-				<button onClick={() => toggleSearch(letters2[scrool[0]])} className='bg-mypink text-mygreen w-24 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl' >
+				<button onClick={() => toggleSearch(letters[scrool[0]])} className='bg-mypink text-mygreen w-24 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl' >
 					<FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
 				<button onClick={() => setScrool(scrool.map(letterNumber => letterNumber == 24 ? 0 : letterNumber += 1))} className='bg-mypink text-mygreen w-24 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl'>
