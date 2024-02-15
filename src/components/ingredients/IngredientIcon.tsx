af5a405e51e880050e0753a2500997be81f1c902
David92p <dave.panetta92@gmail.com>
2024-02-15 12:45:03 +0100

    Fixed css

--- src/components/ingredients/IngredientIcon.tsx
+++ src/components/ingredients/IngredientIcon.tsx
@@ -6,7 +6,7 @@ const IngredientIcon:React.FC<IngredientType> = ({id, name, image}) => {
   const navigate = useNavigate()
 
   return (
-    <div onClick={() => navigate(`/ingredienti/dettaglio/${id}`)} className='flex flex-col items-center gap-4 rounded-xl w-40 sm:w-52 h-auto p-2 bg-mypink border-2 border-blue-500'>
+    <div onClick={() => navigate(`/ingredienti/dettaglio/${id}`)} className='flex flex-col items-center gap-4 rounded-xl w-40 sm:w-52 h-auto p-2 bg-mypink cursor-pointer'>
       <img src={image} alt={name} className='w-20 sm:w-28 2xl:w-36 h-20 sm:h-28 2xl:h-36' />
       <p className="text-mygreen text-center text-xl sm:text-2xl 2xl:text-3xl tracking-wider font-bold">{name}</p>
     </div>
