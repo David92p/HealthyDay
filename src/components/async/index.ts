39534be4d7b71f9dac8e07887b019b8921ac4b98
David92p <dave.panetta92@gmail.com>
2024-02-15 16:47:49 +0100

    added import

--- src/components/async/index.ts
+++ src/components/async/index.ts
@@ -2,12 +2,13 @@ import { getCardsRecipes } from "./recipes"
 import { getResearchdata } from "./research"
 import { getIngredients } from "./ingredients"
 import { getRecipeDetails, getSimilarRecipes } from "./details" 
+import { getIngredientDetails } from "./details" 
 
 import type { SearchType } from "./research" 
 import type { IngredientType } from "./ingredients" 
 
 
-export { getCardsRecipes, getResearchdata, getIngredients, getRecipeDetails, getSimilarRecipes }
+export { getCardsRecipes, getResearchdata, getIngredients, getRecipeDetails, getSimilarRecipes, getIngredientDetails }
 export type { SearchType, IngredientType }
 
 
