8b3168f0aabeedd542b779030c44044f55f735f9
David92p <dave.panetta92@gmail.com>
2024-02-15 16:49:39 +0100

    Added print result for momentary data analysis

--- src/components/details/IngredientDetails.tsx
+++ src/components/details/IngredientDetails.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { getIngredientDetails } from '../async';
 
 const IngredientDetails:React.FC = () => {
-
+  
 	const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    id && getIngredientDetails(import.meta.env.VITE_APP_API_KEY, +id)
+    .then(res => {
+      console.log(res)
+    })
+  }, [id])
 	
   return (
     <div>
