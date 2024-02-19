27a4edd68fb807eee8314ee46135865012721fa3
David92p <Dave.panetta92@gmail.com>
2024-02-19 20:41:07 +0100

    added new imports

--- src/components/details/index.ts
+++ src/components/details/index.ts
@@ -1,9 +1,11 @@
 import RecipeDetails from "./RecipeDetails";
 import ToolLabel from "./ToolLabel";
 import IngredientDetails from "./IngredientDetails";
+import Chart from "./Chart";
 
 import type { RecipeDetailsType } from "./RecipeDetails";
 import type { RecipeToolType } from "./ToolLabel"; 
+import type { IngredientDetailsType } from "./IngredientDetails";
 
-export { RecipeDetails, ToolLabel, IngredientDetails }
-export type { RecipeToolType, RecipeDetailsType }
+export { RecipeDetails, ToolLabel, IngredientDetails, Chart }
+export type { RecipeToolType, RecipeDetailsType, IngredientDetailsType }
