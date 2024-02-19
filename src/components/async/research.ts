1a13abbfb951039a052a3d5c0521218d867e34ef
David92p <Dave.panetta92@gmail.com>
2024-02-19 20:40:52 +0100

    Fixed link api and added meta information

--- src/components/async/research.ts
+++ src/components/async/research.ts
@@ -17,12 +17,11 @@ export const getResearchdata = async (key:string, type: "ingredients" | "recipe"
           data.push({id, title})
         })
       } else {
-        const response =  await axios(`https://api.spoonacular.com/food/ingredients/search?apiKey=${key}&number=8&query=${text}`)
-        response.data.results.map((ingredient: {id: number, name: string}) => {
+        const response =  await axios(`https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${key}&query=${text}&number=8&metaInformation=true`)
+        response.data.map((ingredient: {id: number, name: string}) => {
           const { id, name } = ingredient
           data.push({id,  title: name})
         })
-
       }
       return data
     } catch (error) {
