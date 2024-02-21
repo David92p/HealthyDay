4d79342be1783365ab5bfcb5f0c24d4be759a6f6
David92p <dave.panetta92@gmail.com>
2024-02-21 16:50:19 +0100

    Added imports

--- src/components/details/index.ts
+++ src/components/details/index.ts
@@ -2,10 +2,11 @@ import RecipeDetails from "./RecipeDetails";
 import ToolLabel from "./ToolLabel";
 import IngredientDetails from "./IngredientDetails";
 import Chart from "./Chart";
+import NutritionalValue from "./NutritionalValue";
 
 import type { RecipeDetailsType } from "./RecipeDetails";
 import type { RecipeToolType } from "./ToolLabel"; 
 import type { IngredientDetailsType } from "./IngredientDetails";
 
-export { RecipeDetails, ToolLabel, IngredientDetails, Chart }
+export { RecipeDetails, ToolLabel, IngredientDetails, Chart, NutritionalValue }
 export type { RecipeToolType, RecipeDetailsType, IngredientDetailsType }
