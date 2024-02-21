3e64c0a946306c9d4965c0cb5b58ec93280051da
David92p <dave.panetta92@gmail.com>
2024-02-21 16:51:57 +0100

    Added ingredient label from component

--- src/components/details/IngredientDetails.tsx
+++ src/components/details/IngredientDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Chart } from '.';
+import { NutritionalValue } from '.';
 import { Loading, Error } from "../global"
 import { useParams } from 'react-router-dom';
 import { getIngredientDetails } from '../async';
@@ -9,7 +9,7 @@ export type Nutrition = {
   portion: { amount: number, unit: string }
   caloricBreakdown: { percentProtein: number, percentFat: number, percentCarbs: number }
   flavonoids: { name: string, amount: number, unit: string }[]
-  nutrients: { name: string, amount: number, unit: string, percentOfDailyNeeds: number }
+  nutrients: { name: string, amount: number, unit: string, percentOfDailyNeeds: number }[]
   //properties: { name: string, amount: number, unit: string}
 }
 
@@ -54,13 +54,17 @@ const IngredientDetails:React.FC = () => {
           ? <Loading /> 
           : (
             <>
-              <div className='flex flex-col bg-mygreen border-4 border-blue-500' style={{fontFamily: "Salsa"}}>
-                <div className='flex flex-col w-full p-4 border-2 border-red-500'>
-                  <img src={details?.image} alt={details?.name} className='w-full h-full object-cover'/>
-                  <p className='text-mypink text-3xl sm:text-4xl 2xl:text-7xl font-bold tracking-wider text-center pt-2'>{details?.name}</p>
-                </div>
-                <div className='w-full border-8 border-blue-400'>
-                  { details?.nutrition.caloricBreakdown && <Chart {...details?.nutrition.caloricBreakdown}/> }
+              <div className='flex flex-col bg-mygreen' style={{fontFamily: "Salsa"}}>
+                <div className='flex flex-col w-full items-center p-4 sm:p-6 2xl:p-10'>
+                  <div className='flex flex-col sm:flex-row w-full'>
+                    <div className='sm:w-1/2 2xl:w-1/3 sm:h-[300px] 2xl:h-[400px]'>
+                      <img src={details?.image} alt={details?.name} className="object-contain h-56 sm:h-full w-96 sm:w-full sm:align-middle" />
+                    </div>
+                    <p className='text-mypink text-4xl sm:text-5xl 2xl:text-7xl font-bold tracking-wider text-center mt-4'>{details?.name}</p>
+                  </div>
+                  <div className='flex w-full my-4 sm:my-6 2xl:my-10'>
+                    { details?.nutrition && <NutritionalValue units={details.nutrition.nutrients} portion={details.nutrition.portion}/> }
+                  </div>
                 </div>
               </div>
             </>
