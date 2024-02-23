b1c9c3d01e03df99687d7775464d02ea6be46d14
David92p <dave.panetta92@gmail.com>
2024-02-23 12:03:22 +0100

    Fixed css

--- src/components/ingredients/MainIngredients.tsx
+++ src/components/ingredients/MainIngredients.tsx
@@ -4,14 +4,14 @@ const MainIngredients:React.FC = () => {
   return (
     <div className='flex flex-col h-auto w-full bg-pink-100' style={{fontFamily: "Salsa"}}>
 			<div className='flex flex-col sm:flex-row sm:py-6 2xl:h-[600px]'>
-				<div className='pt-4 px-4 sm:p-6'>
+				<div className='pt-4 px-4 sm:p-6 sm:w-3/5'>
 					<p className='text-mygreen text-3xl sm:text-5xl 2xl:text-7xl tracking-wider'>Cerca i tuoi ingredienti e scegli la semplicità!</p>
 					<p className='text-mygreen tracking-wider text-xl sm:text-3xl mt-4'>
 						Seleziona i tuoi ingredienti iniziando da quì, ascolta il tuo corpo e scegli il meglio con il nostro aiuto!
 						Ci impegnamo ogni giorno a fornirvi ogni dettaglio dei nostri ingredienti, per portare sulle vostre tavole il gusto della salute.
 					</p>
 				</div>
-				<div className='p-4 sm:p-6 2xl:w-3/5'>
+				<div className='p-4 sm:p-6 sm:w-2/5'>
 					<img src={first} alt="first photo" className='h-full w-full object-cover brightness-100 2xl:brightness-75'/>
 				</div>
 			</div>
