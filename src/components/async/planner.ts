feb20bbd3471a52f85c8832fecca7275c276335d
David92p <dave.panetta92@gmail.com>
2024-03-02 14:59:03 +0100

    Fixed fnc link api

--- src/components/async/planner.ts
+++ src/components/async/planner.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-export const generateMealPlan = async (key: string, timeFrame: string) => {
+export const generateMealPlan = async (key: string, timeFrame:string, targetCalories: string, diet: string) => {
     try {
-        const response = await axios(`https://api.spoonacular.com/mealplanner/generate?apiKey=${key}&timeFrame=${timeFrame}`)
+        const response = await axios(`https://api.spoonacular.com/mealplanner/generate?timeFrame=${timeFrame}&targetCalories=${targetCalories}&diet=${diet}&apiKey=${key}`)
         const data = response.data
         return data 
       } catch (error) {
