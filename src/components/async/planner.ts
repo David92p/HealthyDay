13093fce2250b73bc45eec038f0aa0903f30c451
David92p <Dave.panetta92@gmail.com>
2024-03-04 19:41:42 +0100

    Fixed api call with power plan parameters

--- src/components/async/planner.ts
+++ src/components/async/planner.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-export const generateMealPlan = async (key: string, timeFrame:string, targetCalories: string, diet: string) => {
+export const generateMealPlan = async (key: string, timeFrame:string, targetCalories: string, diet: string, exclude:string) => {
     try {
-        const response = await axios(`https://api.spoonacular.com/mealplanner/generate?timeFrame=${timeFrame}&targetCalories=${targetCalories}&diet=${diet}&apiKey=${key}`)
+        const response = await axios(`https://api.spoonacular.com/mealplanner/generate?timeFrame=${timeFrame}&targetCalories=${targetCalories}&diet=${diet}&exclude=${exclude}&apiKey=${key}`)
         const data = response.data
         return data 
       } catch (error) {
