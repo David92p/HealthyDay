7e7b04c962d8242061fdeb51b28204702c025de4
David92p <Dave.panetta92@gmail.com>
2024-03-04 19:46:09 +0100

    Added api call via the generateMealPlan fnc

--- src/components/planner/FoodPlan.tsx
+++ src/components/planner/FoodPlan.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { PlanParametersType } from '.'
+import { generateMealPlan } from '../async'
 
-const FoodPlan:React.FC = () => {
+type FoodPlanType = {
+  parameters: PlanParametersType
+}
+
+const FoodPlan:React.FC<FoodPlanType> = ({parameters}) => {
+
+  useEffect(() => {
+    console.log(parameters)
+  }, [parameters])
+
+  const cercaDati = (parameters: PlanParametersType) => {
+    const { timeFrame, targetCalories, diet, exclude } = parameters
+    generateMealPlan(import.meta.env.VITE_APP_API_KEY, timeFrame, targetCalories, diet, exclude)
+    .then((res) => {
+      console.log(res) 
+    })
+  }
+
   return (
-    <div>
+    <div className='w-full h-72 bg-red-200'>
       FOODPLAN
+      <button onClick={() => cercaDati(parameters)}>CLick</button>
     </div>
   )
 }
