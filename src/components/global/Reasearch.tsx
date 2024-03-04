e8fce7cf8e36e111260e79bf6eec918fbd128ccf
David92p <Dave.panetta92@gmail.com>
2024-03-04 19:44:21 +0100

    Added functionality to search for ingredients to exclude in the nutrition plan

--- src/components/global/Reasearch.tsx
+++ src/components/global/Reasearch.tsx
@@ -7,10 +7,11 @@ import { useNavigate } from 'react-router-dom'
 
 type ResearchType = {
   title: "un ingrediente" | "una ricetta"
-  type: "ingredients" | "recipe"
+  type: "ingredients" | "recipe" | "exclude"
+  setIngredientToExclude?: React.Dispatch<React.SetStateAction<string>>
 }
 
-const Research:React.FC<ResearchType> = ({ title, type }) => {
+const Research:React.FC<ResearchType> = ({ title, type, setIngredientToExclude }) => {
 
   const input = useRef<HTMLInputElement | null>(null)!
   const navigate = useNavigate()
@@ -38,10 +39,10 @@ const Research:React.FC<ResearchType> = ({ title, type }) => {
   }, [input, search])
 
   return (
-    <div className='flex flex-col h-[200px] cursor-default bg-mygreen' style={{fontFamily: "Salsa"}}>
+    <div className={`flex flex-col ${type == "exclude" ? "h-[200px]" : "h-[200px]"} gap-4 cursor-default`} style={{fontFamily: "Salsa"}}>
       <label className='text-3xl sm:text-4xl tracking-wider text-center text-mypink mt-4'>Cerca {title}</label>
       <div className='flex justify-center items-center w-full h-1/3 relative'>
-        <input ref={input} onChange={toggleResearch} type="text"placeholder={`Pasta pomodoro ...`} maxLength={30} className={`h-10 sm:w-full 2xl:w-3/4 bg-slate-100 text-mygreen text-3xl px-2 placeholder:text-2xl py-2 mx-auto sm:mx-4 rounded-md`}/>
+        <input ref={input} onChange={toggleResearch} type="text"placeholder={`Pasta pomodoro ...`} maxLength={30} className={`h-10 w-[90%] sm:w-full 2xl:w-3/4 bg-slate-100 text-mygreen text-3xl px-2 placeholder:text-xl py-2 mx-auto sm:mx-4 rounded-md`}/>
         {
           isLoading 
           ? (
@@ -52,13 +53,14 @@ const Research:React.FC<ResearchType> = ({ title, type }) => {
           : (
             input.current?.value && searchList 
             ? (
-              <div className='flex flex-col w-full 2xl:w-3/4 px-10 sm:px-6 2xl:px-2 pb-2 absolute top-full z-10 tracking-wider bg-mygreen text-slate-100 bg-opacity-75'>
+              <div className='flex flex-col w-full 2xl:w-3/4 px-10 sm:px-6 2xl:px-2 pb-2 absolute top-full z-20 tracking-wider bg-mygreen text-slate-100 bg-opacity-75'>
                 { 
                   searchList.map(el => {
                     return (
                       <div 
-                        onClick={() => 
-                          setSearch({id: el.id, title: el.title})
+                        onClick={type == "exclude" 
+                        ? () => setIngredientToExclude && setIngredientToExclude(el.title)
+                        : () => setSearch({id: el.id, title: el.title})
                         } 
                         key={el.id} 
                         className='text-xl sm:text-3xl tracking-wider 2xl:hover:bg-mypink 2xl:hover:text-mygreen'>
@@ -72,17 +74,22 @@ const Research:React.FC<ResearchType> = ({ title, type }) => {
           )
         }
       </div>
-      <div className='flex justify-center items-center w-full h-1/3'>
-        <button 
-          onClick={() => (
-            search ? (
-              type == "recipe" ? navigate(`/ricettario/dettaglio/${search?.id}`) : navigate(`/ingredienti/dettaglio/${search?.id}`)
-            ) : null 
-          )} 
-          className='bg-mypink text-mygreen w-36 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl' >
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
-      </div>
+      {
+        type != "exclude"
+        ? (
+          <div className='flex justify-center items-center w-full h-1/3 z-10'>
+          <button 
+            onClick={() => (
+              search ? (
+                type == "recipe" ? navigate(`/ricettario/dettaglio/${search?.id}`) : navigate(`/ingredienti/dettaglio/${search?.id}`)
+              ) : null 
+            )} 
+            className='bg-mypink text-mygreen w-36 sm:w-40 h-8 sm:h-10 rounded-sm text-2xl sm:text-3xl' >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        ) : null
+      }
     </div>
   )
 }
