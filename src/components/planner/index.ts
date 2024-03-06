8495fb366e52992c4105a7596835f15333c7092c
David92p <dave.panetta92@gmail.com>
2024-03-06 16:02:34 +0100

    Added import

--- src/components/planner/index.ts
+++ src/components/planner/index.ts
@@ -6,10 +6,11 @@ import TargetCalories from "./TargetCalories";
 import Diet from "./Diet";
 import Exclude from "./Exclude";
 import FoodPlan from "./FoodPlan";
+import FoodPlanOrganization from "./FoodPlanOrganization";
 
 import type { SequenceType, PlanParametersType } from "./Planner";
 import type { MealType, DailyPlanType } from "./FoodPlan";
 
 
-export { Planner, Header, Start, TimeFrame, TargetCalories, Diet, Exclude, FoodPlan }
+export { Planner, Header, Start, TimeFrame, TargetCalories, Diet, Exclude, FoodPlan, FoodPlanOrganization }
 export type { SequenceType, PlanParametersType, MealType, DailyPlanType }
