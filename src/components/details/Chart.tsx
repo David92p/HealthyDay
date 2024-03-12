db4d67d9240fe1d76f4df933e83dac15a7b872c3
David92p <dave.panetta92@gmail.com>
2024-03-12 15:12:36 +0100

    Added toggle function to the chart

--- src/components/details/Chart.tsx
+++ src/components/details/Chart.tsx
@@ -1,22 +1,57 @@
-import React from 'react'
+import { MouseEvent, useEffect, useRef, useState } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut, getElementsAtEvent} from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 type ChartType = {
-	percentProtein: number, percentFat: number, percentCarbs: number
+	type: "percentage" | "grams"
+	percentProteins: number, percentFats: number, percentCarbs: number
+}
+
+type ValuesType = {
+	label : "Proteins" | "Fats" | "Carbs"
+	value: number
 }
 
 
-const Chart:React.FC<ChartType> = ({ percentProtein, percentFat, percentCarbs}) => {
+const Chart:React.FC<ChartType> = ({ type, percentProteins, percentFats, percentCarbs}) => {
+	const updateValues = ():ValuesType => {
+		return {
+			label: percentProteins >= percentFats 
+		? (
+			percentProteins >= percentCarbs 
+			? "Proteins"
+			: "Carbs"
+		) 
+		: (
+			percentFats >= percentCarbs 
+			? "Fats" : "Carbs"
+		), 
+		value: 
+			percentProteins >= percentFats 
+			? (
+				percentProteins >= percentCarbs 
+				? percentProteins
+				: percentCarbs
+			) 
+			: (
+				percentFats >= percentCarbs 
+				? percentFats : percentCarbs
+			)
+		}
+	}
+	const [values, setValues] = useState<ValuesType | null>(null)
+
+
 	const data = {
-		labels: ['Protein', 'Fat', "Carbs"],
+		labels: ['Proteins', 'Fats', "Carbs"],
 		datasets: [
 			{
+				label: "Total",
 				data: [
-					percentProtein, 
-					percentFat, 
+					percentProteins, 
+					percentFats, 
 					percentCarbs 
 				],
 				backgroundColor: [
@@ -34,10 +69,37 @@ const Chart:React.FC<ChartType> = ({ percentProtein, percentFat, percentCarbs}) 
 			},
 		],
 	};
+	
+	const chartRef = useRef()
+
+	const toggleDoughnut = (e:MouseEvent<HTMLCanvasElement, globalThis.MouseEvent>) => {
+		if (chartRef.current && getElementsAtEvent(chartRef.current, e).length > 0 ) {
+			const dataPoint = getElementsAtEvent(chartRef.current, e)[0].index
+			console.log(dataPoint)
+			
+			setValues({label: dataPoint == 0 ? "Proteins" : (dataPoint == 1 ? "Fats" : "Carbs"), value: data.datasets[0].data[dataPoint]})
+		}
+	}
+
+	useEffect(() => {
+		setValues(updateValues())
+	}, [percentCarbs, percentFats, percentProteins])
 
   return (
-		<div className='flex justify-center items-center w-full 2xl:h-[700px] py-4 sm:pl-6'>
-			<Doughnut data={data} />
+		<div className='flex justify-center items-center 2xl:h-[700px] 2xl:mt-10 relative'>
+			<Doughnut data={data} onClick={(e) => toggleDoughnut(e)} ref={chartRef} className='z-10'/>
+			<div className='flex flex-col justify-center items-center h-full w-full absolute 2xl:gap-4'>
+				{
+					values && (
+						<>
+							<span className='text-mypink text-2xl sm:text-3xl 2xl:text-6xl font-bold tracking-wider mt-4 2xl:mt-6'>{values.label}</span>
+							<span className='text-mypink text-3xl sm:text-4xl 2xl:text-6xl font-bold tracking-wider '>
+								{values.value} {type == "percentage" ? "%" : "g"}
+							</span>
+						</>
+					)
+				}
+			</div>
 		</div>
 	)
 }
