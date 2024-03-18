e82a7be3a43a756048aaab6a5de683131b0400cc
David92p <Dave.panetta92@gmail.com>
2024-03-18 20:12:03 +0100

    Added functionality to extend label via props - removed local state for label extension

--- src/components/details/NutritionalValue.tsx
+++ src/components/details/NutritionalValue.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
 
 type NutritionalValueType = {
   title: string
   units: {name: string, amount: number, unit: string, percentOfDailyNeeds?: number}[]
   portion: {amount: number, unit: string}
+  allValues: boolean // boolean se mostrare tutti i valori a schermo 
+  name: "nutritionFacts" | "flavonoidsFacts" // titolo della label
+  showAllValues: (titleLabel: "nutritionFacts" | "flavonoidsFacts") => void
 }
 
-const NutritionalValue:React.FC<NutritionalValueType> = ({title, units, portion}) => {
+const NutritionalValue:React.FC<NutritionalValueType> = ({title, units, portion, allValues, name,  showAllValues}) => {
 
-  const [allValues, setAllValues] = useState<boolean>(false)
-
-	return (
-    <div className='flex flex-col w-full sm:w-1/2 2xl:w-1/3 h-auto bg-pink-100' style={{fontFamily: "Salsa"}}>
+  return (
+    <div className={`flex flex-col w-full sm:w-1/2 2xl:w-1/3 h-1/2 ${allValues ? "sm:absolute top-72 2xl:top-[550px] sm:left-24 2xl:left-44 sm:w-4/5 2xl:w-4/5 sm:h-auto transition-all z-10" : null} bg-pink-100`} style={{fontFamily: "Salsa"}}>
       <p className='text-mygreen font-bold text-center text-3xl sm:text-2xl 2xl:text-3xl tracking-wider py-4 border-b-8 border-mypink mx-2 2xl:pb-7'>{title}</p>
       <div className={`p-4 text-3xl sm:text-2xl 2xl:text-3xl tracking-wider text-mygreen ${units[0].percentOfDailyNeeds ? "text-left" : "text-center"}`}>
         Amount per { portion.amount } { portion.unit }
@@ -22,21 +22,24 @@ const NutritionalValue:React.FC<NutritionalValueType> = ({title, units, portion}
       }
       <div className='flex flex-col'>
         {
-          allValues
-          ? (
-            units.map((value, index) => {
-              return (
-                <div className='flex my-2 border-b-4 border-mypink mx-4 pb-2' key={index}>
-                  <div className='flex justify-center items-center text-center w-3/5 text-xl sm:text-2xl 2xl:text-3xl tracking-wider text-mygreen'>
-                    {value.name} <br /> {value.amount} {value.unit}
-                  </div>
-                  <div className='flex justify-end items-center text-center py-2 w-2/5 text-xl sm:text-2xl 2xl:text-3xl tracking-wider text-mygreen'>
-                    {value.percentOfDailyNeeds} %
-                  </div>
-                </div>
-              )
-            })
-          ) 
+            allValues ? (
+              <div className='grid grid-cols-1 sm:grid-cols-2'>
+              {
+                units.map((value, index) => {
+                  return (
+                    <div className='flex my-2 border-b-4 border-mypink mx-4 pb-2' key={index}>
+                      <div className={`flex justify-center items-center text-center ${units[0].percentOfDailyNeeds ? "w-3/5" : "w-full"} text-xl sm:text-2xl 2xl:text-3xl tracking-wider text-mygreen`}>
+                        {value.name} <br /> {value.amount} {value.unit}
+                      </div>
+                      {units[0].percentOfDailyNeeds ? <div className='flex justify-end items-center text-center py-2 w-2/5 text-xl sm:text-2xl 2xl:text-3xl tracking-wider text-mygreen'>
+                        {value.percentOfDailyNeeds} %
+                      </div> : null}
+                    </div>
+                  )
+                })
+              }
+            </div>   
+            )      
           : (
             units && units.map((value, index) => index < 5 ? (
               <div className='flex my-2 border-b-4 border-mypink mx-4 pb-2' key={index}>
@@ -53,8 +56,10 @@ const NutritionalValue:React.FC<NutritionalValueType> = ({title, units, portion}
         {
           units.length > 5 ? (
             <div className='flex justify-around w-full py-4'>
-            <button className='bg-mypink text-mygreen text-lg px-6 py-1 tracking-wider' onClick={() => setAllValues(!allValues)}>
-              { allValues ? "Close" : "More" }
+            <button className='bg-mypink text-mygreen text-lg px-6 py-1 tracking-wider' 
+              onClick={() => showAllValues(name)}
+            >
+              { allValues  ? "Close" : "More" }
             </button>
           </div>
           ) : null
