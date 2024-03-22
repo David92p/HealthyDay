349eb771a508b2ac439b7caf8e99783596bb389c
David92p <dave.panetta92@gmail.com>
2024-03-22 11:52:23 +0100

    Fixed css

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Loading:React.FC = () => {
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-screen bg-mygreen'>
       <div className='flex flex-col justify-center items-center gap-6 w-full h-1/3 2xl:h-2/3 mt-10 sm:mt-36 2xl:my-auto text-mypink'>
         <FontAwesomeIcon icon={faSpinner} spin className='text-6xl sm:text-8xl'/>
         <h1 className='text-5xl sm:text-7xl tracking-wider'>Loading ...</h1>
