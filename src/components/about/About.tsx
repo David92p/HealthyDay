604393e28286f22347e8bedaa4db19d56278bbe9
David92p <dave.panetta92@gmail.com>
2024-03-22 11:20:38 +0100

    Changed text and added comp. Redirection

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,36 +1,29 @@
 import React from 'react'
-
 import about1 from "../../assets/about/about1.jpg"
-import { useNavigate } from 'react-router-dom'
+import { Redirection } from '../global'
 
 const About:React.FC = () => {
 
-    const navigate = useNavigate()
-
     return (
-    <div className='flex flex-col w-full h-auto cursor-default' style={{fontFamily: "Salsa"}}>
+    <div className='flex flex-col w-full h-auto cursor-default bg-mygreen' style={{fontFamily: "Salsa"}}>
         {/* Header */}
-        <div className='flex flex-col sm:flex-row-reverse sm:h-[450px] 2xl:h-[600px] w-full items-center px-4 2xl:px-12 py-4 bg-mygreen'>
+        <div className='flex flex-col sm:flex-row-reverse sm:h-[450px] 2xl:h-[600px] w-full items-center px-4 2xl:px-12 py-4 text-slate-100'>
             <img src={about1} alt="About1" className='brightness-75 2xl:brightness-50 object-cover h-full w-full sm:w-1/3 2xl:w-2/3'/>
             <p className='text-slate-100 text-center text-lg sm:text-2xl pt-4 sm:pt-0 sm:mr-4 2xl:mr-20 tracking-wider'>
-                Buongiorno Salute nasce dall'idea di portare sulle nostre tavole tutto ciò che la terra ha da offrirci.<br/>
-                Pensiamo che ogni singolo prodotto creato dal nostro pianeta e dai suoi essere viventi sia un dono, offerto per aiutarci a trovare il perfetto equilibrio tra corpo e salute.<br/>
-                Scroprire tutte le nostre ricette e prenderne spunto  sarà il primo passo verso per raggiungere il perfetto equilibrio tra corpo e salute.
+                Healthy Day was born from the idea of bringing the taste of our cuisine to our tables.
+                We think that every single product on our planet is a gift, offered to help us find the perfect balance between body and health. Our recipes are always healthy meals created with love but leaving the choice of diet to follow up to you .<br/>
+                Our advice is always to follow a healthy and balanced diet, always favoring a good amount of fruit and vegetables.<br/>
+                Don't forget to follow the advice of a nutritionist before embarking on a long-term diet.
             </p>
         </div>
         {/* buttons collegamenti */}
-        <div className='flex flex-col 2xl:h-[400px] justify-center 2xl:justify-around gap-6 items-center py-10 bg-mypink tracking-wider'>
-            <p className='text-3xl 2xl:text-5xl text-center tracking-wider'>Consulta subito i <br />nostri piani</p>
-            <div className='flex w-full flex-wrap justify-center gap-4 px-4'>
-                <button onClick={() => navigate("/ingredienti")} className='bg-mygreen text-mypink px-auto w-32 sm:w-40 2xl:w-48 h-10 sm:h-10 2xl:h-14 rounded-sm text-xl sm:text-3xl' >Ingredienti</button>
-                <button onClick={() => navigate("/ricettario")} className='bg-mygreen text-mypink px-auto w-32 sm:w-40 2xl:w-48 h-10 sm:h-10 2xl:h-14 rounded-sm text-xl sm:text-3xl' >Ricettario</button>
-                <button onClick={() => navigate("/pianifica")} className='bg-mygreen text-mypink px-auto w-32 sm:w-40 2xl:w-48 h-10 sm:h-10 2xl:h-14 rounded-sm text-xl sm:text-3xl' >Pianifica</button>
-            </div>
-        </div>
+        <Redirection type={"about"} />
         {/* Footer */}
         <div className='w-full sm:h-[400px]  bg-mygreen text-slate-100 text-center p-4 sm:p-10 2xl:p-24 text-lg sm:text-2xl tracking-wider'>
-            E' posizione dell'Academy of Nutrition and Dietetics che le diete vegetariane correttamente pianificate, comprese le diete totalmente vegetariane o vegane, sono salutari, nutrizionalmente adeguate e possono apportare benefici per la salute nella prevenzione e nel trattamento<br/> di alcune patologie.<br/>
-            Queste diete sono adatte in tutti gli stadi del ciclo vitale, inclusi la gravidanza, l'allattamento, la prima e la seconda infanzia, l'adolescenza, l'età adulta, per gli anziani e per gli atleti.
+            On behalf of Healthy Day I would like to thank the data service provided by Spoonacular.<br/> 
+            This web app was created for educational purposes only in order to enhance my development knowledge.<br/>
+            If you think you want to make some changes or improvements please contact me via the form and visit my GitHub profile in the contacts.<br/>
+            For any collaboration I remain available, a healthy greeting from Healty Day!
         </div>
     </div>
   )
