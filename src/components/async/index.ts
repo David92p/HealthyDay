d2e7a067f38c6173d56ab67219c791be088a1305
David92p <dave.panetta92@gmail.com>
2024-03-28 12:26:02 +0100

    Added imports

--- src/components/async/index.ts
+++ src/components/async/index.ts
@@ -1,13 +1,13 @@
 import { getCardsRecipes } from "./recipes"
 import { getResearchdata } from "./research"
-import { getIngredients } from "./ingredients"
-import { getRecipeDetails, getSimilarRecipes, getIngredientDetails, getIngredientSubstitutes } from "./details" 
-import { generateMealPlan } from "./planner"
+import { getIngredients, getIngredientSubstitutes } from "./ingredients"
+import { getRecipeDetails, getSimilarRecipes, getIngredientDetails } from "./details" 
+import { getMealPlan } from "./planner"
 
 import type { SearchType } from "./research" 
 import type { IngredientType } from "./ingredients" 
 
-export { getCardsRecipes, getResearchdata, getIngredients, getRecipeDetails, getSimilarRecipes, getIngredientDetails, getIngredientSubstitutes, generateMealPlan }
+export { getCardsRecipes, getResearchdata, getIngredients, getIngredientSubstitutes, getRecipeDetails, getSimilarRecipes, getIngredientDetails, getMealPlan }
 export type { SearchType, IngredientType }
 
 
